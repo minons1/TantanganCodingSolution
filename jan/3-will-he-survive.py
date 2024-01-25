670a696313c0df35676da86508ad73dff869d1cb
minons1 <salimbinusman1@gmail.com>
2024-01-25 22:45:40 +0700

    feat: prob 3 & 4

---
+++ jan/3-will-he-survive.py
@@ -0,0 +1,7 @@
+def hero(bullets, dragons):
+    print(bullets/2, bullets // 2)
+    return bullets / 2 >= dragons
+
+
+hero(10, 3)
+hero(7, 4)
---
+++ jan/4-sentence-smash.py
@@ -0,0 +1,7 @@
+def smash(words):
+    return ' '.join(words)
+
+
+print(smash(['hello', 'this', 'is', 'me', 'trying', 'solve', 'this', 'problem']))
+print(smash([]))
+print(smash(['oneword']))
