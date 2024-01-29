6aaa38743ada561fbac8c14c4e5cbd5de2db3cb6
minons1 <salimbinusman1@gmail.com>
2024-01-29 22:21:07 +0700

    leet: 3

---
+++ leetcode/3-longest-substring-without-repeating-charachter.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+          substring = ''
+          max_len = 0
+          for char in s:
+              if char not in substring:
+                substring = substring + char
+              else:
+                  substring = substring[substring.index(char) + 1:] + char
+              cur_len = len(substring)
+              max_len = cur_len if cur_len > max_len else max_len
+              print(substring)
+          return max_len
+
+sol = Solution()
+print(sol.lengthOfLongestSubstring('salim'))
+print(sol.lengthOfLongestSubstring('abcabcbb'))
+print(sol.lengthOfLongestSubstring('bbbbb'))
+print(sol.lengthOfLongestSubstring('pwwkew'))
+print(sol.lengthOfLongestSubstring('aabaab!bb'))
