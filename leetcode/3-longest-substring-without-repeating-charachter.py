5424c1b8f4f677395661ca071e64280a67440e55
minons1 <salimbinusman1@gmail.com>
2024-01-30 22:56:28 +0700

    day 7?

--- leetcode/3-longest-substring-without-repeating-charachter.py
+++ leetcode/3-longest-substring-without-repeating-charachter.py
@@ -1,15 +1,15 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-          substring = ''
-          max_len = 0
-          for char in s:
-              if char not in substring:
+        substring = ''
+        max_len = 0
+        for char in s:
+            if char not in substring:
                 substring = substring + char
-              else:
-                  substring = substring[substring.index(char) + 1:] + char
-              cur_len = len(substring)
-              max_len = cur_len if cur_len > max_len else max_len
-          return max_len
+            else:
+                substring = substring[substring.index(char) + 1:] + char
+            cur_len = len(substring)
+            max_len = cur_len if cur_len > max_len else max_len
+        return max_len
 
 sol = Solution()
 print(sol.lengthOfLongestSubstring('salim'))
---
+++ leetcode/4-median-of-two-sorted-arrays.py
@@ -0,0 +1,35 @@
+from typing import List
+import math
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        total_len = len(nums1) + len(nums2)
+        is_even = total_len % 2 == 0
+
+
+        last = second_from_last = None
+
+        for i in range(math.floor(total_len / 2) + 1):
+            num1 = nums1[0] if nums1 else None
+            num2 = nums2[0] if nums2 else None
+
+            second_from_last = last
+            if num1 is not None and num2 is None:
+                print('if')
+                last = nums1.pop(0)
+            elif num2 is not None and num1 is None:
+                print('elif')
+                last = nums2.pop(0)
+            else:
+                print('else')
+                last = nums1.pop(0) if num1 <= num2 else nums2.pop(0)
+
+            print(second_from_last, last)
+        
+        return (last + second_from_last) / 2 if is_even else last
+
+sol = Solution()
+# print(sol.findMedianSortedArrays([1,3], [0]))
+# print(sol.findMedianSortedArrays([10], []))
+# print(sol.findMedianSortedArrays([1,3], [2,4]))
+print(sol.findMedianSortedArrays([0,0], [0,0]))
